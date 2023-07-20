54f8b21db42aa674742cb167bfcc98081a25782b
Raselahmed2 <raselahmed199916@gmail.com>
2023-07-20 20:28:12 +0600

    Typescript problem solve

---
+++ Basic-typescript/dist/m-2-advance-type/mapped.js
@@ -0,0 +1,3 @@
+"use strict";
+const arrayOfNumber = [2, 5, 6];
+const area1 = { height: 10, width: 10 };
---
+++ Basic-typescript/dist/m-3-object-oriented/access-modifyer.js
@@ -0,0 +1,13 @@
+"use strict";
+// class BankAccount {
+//     id : number ;
+//     bankAcName : string;
+//     private _balance : number;
+//     constructor(id : number , bankAcName : string , _balance : number){
+//         this.id = id;
+//         this.bankAcName = bankAcName;
+//         this._balance = _balance;
+//     }
+// }
+// const newAccount = new BankAccount (21 , 'Mukut' , 25000) 
+// console.log(newAccount)
